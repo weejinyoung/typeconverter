e52e0ebaff91490166523fce4ed280b4dd6b058f
Jn W <a8118199@gmail.com>
2022-04-27 14:44:54 +0900

    포맷터를 지원하는 컨버전 서비스

---
+++ src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,23 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+public class FormattingConversionServiceTest {
+
+    @Test
+    void formattingConversionServiceTest() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
